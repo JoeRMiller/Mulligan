5b678896afe853f3e41af15c3a19bdbf2607b6ce
JoeRMiller <24966440+JoeRMiller@users.noreply.github.com>
2023-12-09 19:21:54 -0800

    Changed Data Providers to an external PostgreSQL database

--- Mulligan Web/Program.cs
+++ Mulligan Web/Program.cs
@@ -12,8 +12,13 @@ namespace Mulligan.Web
 
             // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            
+            //builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(connectionString));
+
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
+                options.UseNpgsql(connectionString));
+
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
