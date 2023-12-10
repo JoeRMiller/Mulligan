46317d765e7001670474fea35e9534a6fef2ba19
JoeRMiller <24966440+JoeRMiller@users.noreply.github.com>
2023-12-09 19:28:11 -0800

    Added connection string to secrets.json

--- Mulligan Web/Program.cs
+++ Mulligan Web/Program.cs
@@ -11,11 +11,8 @@ namespace Mulligan.Web
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            var connectionString = builder.Configuration.GetConnectionString("MulliganDBConnectionString") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             
-            //builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            //    options.UseSqlServer(connectionString));
-
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseNpgsql(connectionString));
 
