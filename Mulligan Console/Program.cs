bd67b3f39ebb90cb188b95b0fd82f14f92411849
JoeRMiller <24966440+JoeRMiller@users.noreply.github.com>
2023-12-22 23:24:42 -0800

    Imported States CSV

--- Mulligan Console/Program.cs
+++ Mulligan Console/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Mulligan.Console
+﻿using Mulligan.Core.Data;
+using Mulligan.Core.Models;
+
+namespace Mulligan.Console
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello, World!");
+            //@"D:\Development\source\repos\Mulligan\Mulligan Console\states.csv"
+            var importer = new Importer(@"D:\Development\source\repos\Mulligan\Mulligan Console\states.csv");
+            importer.Import();
+            System.Console.WriteLine("Finished");
         }
     }
+
+
 }
